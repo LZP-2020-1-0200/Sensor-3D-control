5489ed88e097cea4ceb4421cc0373b6f05b91627
Scarpy019 <scarpy019@gmail.com>
2023-03-14 11:30:57 +0200

    Stepper class changes with command support

--- include/stepper.h
+++ include/stepper.h
@@ -17,31 +17,48 @@ enum stepper_direction_t
 
 #define MIN_STEP_DELAY_USEC 1000
 #define MAX_STEP_DELAY_USEC 10000000
-
+#define STEPPER_COUNT 3
 class Stepper
 {
 private:
     /* data */
-    int position;
-    stepper_direction_t direction;
+    int position=0;
+    stepper_direction_t direction=STEPPER_PAUSE;
     uint32_t prev_usec;
     uint32_t step_delay_usec;
-
+    volatile uint16_t direction_change_timer=0;
+    volatile uint16_t millisecond_counter = 0;
+    volatile uint16_t delay_milliseconds = 5;
+    volatile uint8_t *port;
+    volatile uint8_t *portRegister;
+    const int stepperPins[4];
+    const uint8_t stepper1_pins = (1 << DDB4) | (1 << DDB5) | (1 << DDB6) | (1 << DDB7);
+    const uint8_t stepper1_mask = ~stepper1_pins;
 public:
-    Stepper(/* args */){};
+    int targetPos=0;
+    int direction_change_steps=0;
+    Stepper(volatile uint8_t* port, volatile uint8_t* portRegister, int pinA, int pinB, int pinC, int pinD)
+        : port(port), portRegister(portRegister), stepperPins({pinA,pinB,pinC, pinD}),
+        stepper1_pins((1<<pinA)|(1<<pinB)|(1<<pinC)|(1<<pinD)), stepper1_mask(~stepper1_pins){
+
+        cli();
+        *portRegister |= stepper1_pins;//add stepper pins as outputs
+        sei();
+    };
     ~Stepper(){};
-    void init(void);
+    static void init(void);
+    static Stepper* motors[STEPPER_COUNT];
 
     int exec(const uint32_t &);
     int get_position(void) const { return position; }
-    void set_direction(stepper_direction_t &dir) { direction = dir; }
+    void set_direction(stepper_direction_t dir);
 //    void set_direction(const char *dstr);
     stepper_direction_t get_direction(void) const { return direction; }
-
+    void inline move();
     uint32_t get_step_delay_usec(void) const { return step_delay_usec; }
     void delay(const char *a);
+    void lock(bool x);
 };
 
-extern Stepper stepper;
 
 #endif /* !STEPPER_H */
