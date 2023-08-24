fbdbfc72250246ac85702fc317872bdc01181f30
Scarpy019 <scarpy019@gmail.com>
2023-08-24 11:46:28 +0300

    SERIAL was taken, swapped to SERIAL_NUMBER

--- include/modelData.h
+++ include/modelData.h
@@ -6,8 +6,8 @@
 // Mirorless assembly for microscopic imaging
 #define MODEL "MAMI"
 #endif
-#ifndef SERIAL
-#define SERIAL "0001"
+#ifndef SERIAL_NUMBER
+#define SERIAL_NUMBER "0001"
 #endif
 #ifndef SOFTWARE_VERSION
 #define SOFTWARE_VERSION "v0.0.1"
