a0656bcfdc1dbbcfd2c01d91a22b84e8eebf5840
Scarpy019 <scarpy019@gmail.com>
2023-08-24 11:54:10 +0300

    copy constructor deletion and serial print

--- include/scpi2.h
+++ include/scpi2.h
@@ -12,18 +12,24 @@ namespace scpi{
     public:
         struct ChildCMD
         {
-            const char* txt;
+            const char* const txt;
             Command* cmd;
+            const size_t txtLen;
+            // delete copy constructor and assignment operator
+            ChildCMD(const ChildCMD& ccmd) : txt(ccmd.txt), cmd(ccmd.cmd), txtLen(ccmd.txtLen) { };
+            ChildCMD& operator=(const ChildCMD&) = delete;
+            ChildCMD(const char* const txt, Command* cmd)
+                : txt(txt), cmd(cmd), txtLen(strlen(txt)){};
         };
     private:
         int (* const func)(int, char**)=nullptr;
         void (* const get)()=nullptr;
         
         int cmdInd(char*, int);
-        const ChildCMD* subcommands;
+        const ChildCMD* const subcommands;
         const size_t childCMDCount;
     public:
-        Command(int (* const func)(int, char**), void (* const get)(), const ChildCMD* subCMD, int childCMDCount)
+        Command(int (* const func)(int, char**), void (* const get)(), const ChildCMD* const subCMD, int childCMDCount)
             : func(func), get(get),
               subcommands(subCMD), childCMDCount(childCMDCount){}
         template<size_t N>
@@ -37,7 +43,10 @@ namespace scpi{
             : func(func), get(nullptr),
               subcommands(nullptr), childCMDCount(0){}
         ~Command(){};
-
+        // delete copy constructor and assignment operator
+        Command(const Command& cmd) = delete;
+        Command& operator=(const Command&) = delete;
+        void dump(void) const;
         void parse(HardwareSerial&, char*);
 
     };
@@ -48,18 +57,12 @@ namespace scpi{
         HardwareSerial& serial;
         char input[SCIPI_BUFFER_SIZE+1];
         int inBufIndex=0;
-        Command cmd;
+        Command& cmd;
         
     public:
-        scpi2(HardwareSerial& serial, Command cmd): serial(serial), cmd(cmd){};
+        scpi2(HardwareSerial& serial, Command& cmd): serial(serial), cmd(cmd){};
         ~scpi2();
 
         void update();
     };
-
-    
-    
-
 }
-
-
--- src/scpi2.cpp
+++ src/scpi2.cpp
@@ -41,6 +41,9 @@ void scpi2::update(){
 }
 
 int Command::cmdInd(char* command, int c){
+    Serial.write("cmdInd: ");
+    Serial.write(command, c);
+    Serial.write("\r\n");
     for(size_t i=0 ; i<childCMDCount ; i++){
         if(strncasecmp(subcommands[i].txt, command, c)==0){
             return i;
@@ -50,6 +53,9 @@ int Command::cmdInd(char* command, int c){
 }
 
 void Command::parse(HardwareSerial& serial, char* line){
+    Serial.print("parse: ");
+    Serial.print(line);
+    Serial.print("\r\n");
     //iterate up to seperator
     for(int i=0;line[i]!='\0';i++){
         //sub cmd
@@ -57,9 +63,9 @@ void Command::parse(HardwareSerial& serial, char* line){
             // check if parent command exists
             int ind = cmdInd(line, i);
             if(ind==-1){
-                serial.write("Command \"");
+                serial.print("Command \"");
                 serial.write(line, i);
-                serial.write("\" not found.\r\n");
+                serial.print("\" not found.\r\n");
                 return;
             }
             //parse subcommand
@@ -71,9 +77,9 @@ void Command::parse(HardwareSerial& serial, char* line){
             //check if command exists
             int ind = cmdInd(line, i);
             if(ind==-1){
-                serial.write("Command \"");
+                serial.print("Command \"");
                 serial.write(line, i);
-                serial.write("\" not found.\r\n");
+                serial.print("\" not found.\r\n");
                 return;
             }
             char* argv[SCIPI_MAX_ARGC];
@@ -94,7 +100,7 @@ void Command::parse(HardwareSerial& serial, char* line){
                         argc++;
                         start=false;
                         if(argc==SCIPI_MAX_ARGC){
-                            serial.write("Too many arguments.\r\n");
+                            serial.print("Too many arguments.\r\n");
                             return;
                         }
                         argv[argc]=line+i+1;
@@ -104,7 +110,7 @@ void Command::parse(HardwareSerial& serial, char* line){
             }while (line[i]!='\0');
             argc++;
             if(subcommands[ind].cmd->func==nullptr){
-                serial.write("Call cmd not supported. Did you mean to add '?'?\r\n");
+                serial.print("Call cmd not supported. Did you mean to add '?'?\r\n");
                 return;
             }
             subcommands[ind].cmd->func(argc, argv);
@@ -114,25 +120,53 @@ void Command::parse(HardwareSerial& serial, char* line){
         else if(line[i]=='?'){
             int ind = cmdInd(line, i);
             if(ind==-1){
-                serial.write("Command \"");
-                serial.write(line);
-                serial.write("\" not found.\r\n");
+                serial.print("Command \"");
+                serial.print(line);
+                serial.print("\" not found.\r\n");
+                return;
+            }
+            if(subcommands[ind].cmd->get==nullptr){
+                serial.print("Get cmd not supported.\r\n");
                 return;
             }
             subcommands[ind].cmd->get();
             return;
         }
     }
-    //call cmd
+    //call cmd without arguments
     int ind = cmdInd(line, strlen(line));
     if(ind==-1){
-        serial.write("Command \"");
-        serial.write(line);
-        serial.write("\" not found.\r\n");
+        serial.print("Command \"");
+        serial.print(line);
+        serial.print("\" not found.\r\n");
+        return;
+    }
+    if(subcommands[ind].cmd->func==nullptr){
+        serial.print("Call cmd not supported. Did you mean to add '?'?\r\n");
         return;
     }
     subcommands[ind].cmd->func(1, &line);
     return;
+}
+
+void Command::dump(void) const {
+    Serial.print("func: ");
+    Serial.print((int)func, HEX);
+    Serial.print(" get: ");
+    Serial.print((int)get, HEX);
+    Serial.print(" subcommand count: ");
+    Serial.print((long)childCMDCount, DEC);
+    Serial.print("\r\n");
+    Serial.print("subcommands: ");
+    Serial.print((int)subcommands, HEX);
+    Serial.print("\r\n");
+    for(size_t i=0 ; i<childCMDCount ; i++){
+        Serial.print(subcommands[i].txt);
+        Serial.print(" start.\r\n");
+        subcommands[i].cmd->dump();
+        Serial.print(subcommands[i].txt);
+        Serial.print(" end.\r\n");
+    }
 }
 
 } // namespace scpi
